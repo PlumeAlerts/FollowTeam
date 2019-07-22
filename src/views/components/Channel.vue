c3594c6727c7fb9f8ab18166480dbde9e3fcd2c4
lclc98 <liam.c14785@gmail.com>
2019-07-22 17:51:41 +1000

    Fixed following

--- src/views/components/Channel.vue
+++ src/views/components/Channel.vue
@@ -1,25 +1,45 @@
 <template>
-  <article class="level" style="border: 1px;">
-    <div class="level-left">
-      <div class="level-item">
-        <figure class="image is-64x64">
-        <img :src="item.logo">
-        </figure>
+  <div class="box" :class="item.follow?'selected':'unselected'" @click="select">
+    <article class="level" style="border: 1px;">
+      <div class="level-left">
+        <div class="level-item">
+          <figure class="image is-64x64">
+            <img class="is-rounded" :src="item.logo">
+          </figure>
+        </div>
+        <div class="level-item">
+          <VueResponsiveText>
+            <b>{{item.display_name}}</b>
+          </VueResponsiveText>
+        </div>
       </div>
-      <div class="level-item">
-        <h1 class="title">{{item.display_name}}</h1>
-      </div>
-    </div>
-  </article>
+    </article>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'Channel',
-  props: ['item'],
-};
+    import VueResponsiveText from 'vue-responsive-text';
+
+    export default {
+        name: 'Channel',
+        props: ['item'],
+        components: {
+            VueResponsiveText,
+        },
+        methods: {
+            select() {
+                this.item.follow = !this.item.follow;
+            },
+        },
+    };
 </script>
 
 <style scoped>
+  .box.selected {
+    background-color: #375a7f !important;
+  }
 
+  .box.unselected {
+    background-color: #e74c3c !important;
+  }
 </style>
